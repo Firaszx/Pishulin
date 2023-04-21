fbfa808876ce7672d69763d92c65b046f0570d92
Firaszx <qqqqqq222333@yandex.ru>
2023-04-22 00:09:16 +0300

    апчхибда

---
+++ Files/ConsoleApp5/Files.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Files
+{
+    internal partial class MyFiles
+    {
+       
+            public static void FileWrite(string path, string input)
+            {
+                using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(input);
+                    fstream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Файл создан");
+                }
+            }
+        public static void FileRead(string path)
+        {
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                fstream.Read(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+        }
+        
+    }
+}
---
+++ Files/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using Files;
+namespace ConsoleApp5
+{
+    internal partial class Program
+    {
+        static void Main()
+        {
+            string path = @"D:\test\note.txt";
+            string input = "1000-7=..." + "\r\n";           
+            for (int i = 1000; i > 1; i=i-7)
+             { 
+                input = input + Convert.ToString(i) + "\r\n";
+            }
+            MyFiles.FileWrite(path, input);
+            MyFiles.FileRead(path);
+            Console.ReadLine();
+
+        }
+    }
+}
