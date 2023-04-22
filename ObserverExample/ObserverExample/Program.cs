669654ac50f3a85f1720d6456870b2ff73043882
Firaszx <qqqqqq222333@yandex.ru>
2023-04-22 22:46:49 +0300

    ObsExample

---
+++ ObserverExample/ObserverExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Observer;
+namespace ObserverExample
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject s = new Subject();
+            s.Event += () => Console.WriteLine("Что-то произошло нате уведомление");
+            s.RaiseEvent();
+            Console.ReadLine();    
+        }
+    }
+}
