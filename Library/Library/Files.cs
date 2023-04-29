417721123ff17c9f2c13b601735061fb95728d22
Firaszx <qqqqqq222333@yandex.ru>
2023-04-29 18:00:09 +0300

    SingleTon

---
+++ Library/Library/Files.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace Lib
+{
+        public class Files
+        {
+            public static void FileWrite(string path, string input)
+            {
+                if (File.Exists(path))
+                {
+                    using (FileStream fstream = new FileStream(path, FileMode.Truncate))
+                    {
+                        byte[] buffer = Encoding.Default.GetBytes(input);
+                        fstream.Write(buffer, 0, buffer.Length);
+                    }
+                }
+                else
+                {
+                    using (FileStream fstream = new FileStream(path, FileMode.Create))
+                    {
+                        byte[] buffer = Encoding.Default.GetBytes(input);
+                        fstream.Write(buffer, 0, buffer.Length);
+                    }
+                }
+            }
+            public static void FileNotReWrite(string path, string input)
+            {
+
+                using (FileStream fstream = new FileStream(path, FileMode.Append))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(input);
+                    fstream.Write(buffer, 0, buffer.Length);
+                }
+
+            }
+            public static string FileRead(string path)
+            {
+                using (FileStream fstream = File.OpenRead(path))
+                {
+                    byte[] buffer = new byte[fstream.Length];
+                    fstream.Read(buffer, 0, buffer.Length);
+                    string textFromFile = Encoding.Default.GetString(buffer);
+                    Console.WriteLine($"Текст из файла: {textFromFile}");
+                    return textFromFile;
+                }
+            }
+        }
+    }
---
+++ Library/Writer/Writer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Libr;
+namespace Writer
+{
+    internal class Writer
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                i++;
+                Files.FileNotReWrite((Singleton.GetPath().path), $"Тут написано{i.ToString()} ");
+            }
+        }
+    }
+}
